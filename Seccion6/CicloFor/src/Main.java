ceec63fd2fc2ba3db60b29e8d586a387e3929d39
jose collado <josecolladobravo@gmail.com>
2022-01-24 10:38:14 +0100

    Añadida Seccion 6

---
+++ Seccion6/CicloFor/src/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Josec
+ */
+public class Main {
+    public static void main(String[] args) {
+        for(var contador=0; contador<3; contador++){
+            System.out.println("contador = " + contador);
+        }
+    }
+}
---
+++ Seccion6/DoWhile/src/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Josec
+ */
+public class Main {
+    public static void main(String[] args) {
+        var contador=0;
+        
+        do{
+            System.out.println("contador = " + contador);
+            contador++;
+        }while(contador<3);
+    }
+}
