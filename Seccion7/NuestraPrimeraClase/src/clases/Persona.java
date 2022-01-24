94e7f47ff1a98d1120ae7604bc7b0c7104fafe49
jose collado <josecolladobravo@gmail.com>
2022-01-24 12:36:14 +0100

    Añadida Seccion 7

---
+++ Seccion7/NuestraPrimeraClase/src/clases/Persona.java
@@ -0,0 +1,16 @@
+package clases;
+
+
+public class Persona {
+    //Atributos de la clase
+    String nombre;
+    String apellido;
+    
+    //Metodos de la clase
+    public void desplegarInformacion(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+    }
+    
+
+}
