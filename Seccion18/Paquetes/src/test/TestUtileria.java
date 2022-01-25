07ec217e35c184143604911db414a2e60f213ab4
jose collado <josecolladobravo@gmail.com>
2022-01-25 21:11:08 +0100

    Añadida Sección 18

---
+++ Seccion18/Paquetes/src/test/TestUtileria.java
@@ -0,0 +1,13 @@
+package test;
+
+//import mx.com.globalmentoring.*;//carga todas las clases. 
+//import mx.com.globalmentoring.Utileria; //importa la clase Utileria del paquete. Metodo 1
+//import static mx.com.globalmentoring.Utileria.imprimir; Metodo 2
+
+public class TestUtileria {
+    public static void main(String[] args) {
+        //Utileria.imprimir("Saludos"); Metodo 1
+        //imprimir("Adios"); Metodo 2
+        //mx.com.globalmentoring.Utileria.imprimir("adios"); //otro metodo 
+    }
+}
