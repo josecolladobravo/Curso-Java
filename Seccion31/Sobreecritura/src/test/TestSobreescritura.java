6959ceb097c754dbe9b8cf42ebe44487169166ab
jose collado <josecolladobravo@gmail.com>
2022-01-28 16:35:03 +0100

    Añadida Sección 31

---
+++ Seccion31/Sobreecritura/src/test/TestSobreescritura.java
@@ -0,0 +1,21 @@
+package test;
+
+import domain.*;
+
+
+public class TestSobreescritura {
+    public static void main(String[] args) {
+        //polimorfimo es multiple comportamientos
+        
+        Empleado empleado = new Empleado("Juan", 5000);
+//        System.out.println("empleado = " + empleado.obtenerDetalles());
+        imprimir(empleado);
+        Gerente gerente = new Gerente("Karla", 10000, "Contabilidad");
+//        System.out.println("gerente = " + gerente.obtenerDetalles());
+        imprimir(gerente);
+    }
+    
+    public static void imprimir(Empleado empleado){ //el empleado que se le pasa tambien puede actuar como gerente
+        System.out.println("empleado = " + empleado.obtenerDetalles());
+    }
+}
