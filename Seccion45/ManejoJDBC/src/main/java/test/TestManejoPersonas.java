f0b911ca68798d0c83776014400afffd12b7a48d
jose collado <josecolladobravo@gmail.com>
2022-02-03 20:10:36 +0100

    Añadida Sección 45

---
+++ Seccion45/ManejoJDBC/src/main/java/test/TestManejoPersonas.java
@@ -0,0 +1,35 @@
+package test;
+
+import datos.*;
+import domain.Persona;
+import java.util.List;
+
+public class TestManejoPersonas {
+    public static void main(String[] args) {
+        PersonaDAO personaDao= new PersonaDAO();
+        
+        
+        
+        //Insertando un nuevo objeto de tipo Persona
+//        Persona personaNueva = new Persona("Carlos", "Esparza", "cesparza@mail.com", "2345252");
+//        personaDao.insertar(personaNueva);
+        
+
+        
+        //Actualizar
+        Persona persona2 = new Persona(4, "Carlitos", "Garcia", "cgarcia@mail.com", "456789023");
+        personaDao.actualizar(persona2);
+        
+
+        
+        //borrar
+        Persona persona3 = new Persona(4);
+        personaDao.borrar(persona3);
+        
+        List<Persona> personas = personaDao.seleccionar();
+        
+        personas.forEach(persona -> {
+            System.out.println("persona = " + persona);
+        });
+    }
+}
